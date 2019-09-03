9781fc318dfc21595b67e3ef6e9b51eea65f415d
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-03 19:14:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,13 +3,12 @@ class Anagram
   attr_accessor :word 
   
   def initialize(word)
-    @word
+    @word = word 
   end 
   def match(listen)
       listen.select do |anag|
       (@word.split("").sort) == (anag.split("").sort)
     binding.pry 
-    end = word
     end 
   end
 end 
